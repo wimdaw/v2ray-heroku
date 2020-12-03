ea853925ea858fea925e7feb01e659a7a6ad1fa5
wimdaw <3272869@qq.com>
2020-12-03 23:04:57 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -30,7 +30,6 @@ cat << EOF > /usr/local/etc/v2ray/config.json
             },
             "streamSettings": {
                 "network": "ws"
-                "security": "tls"
             }
         }
     ],
